096820dbca70c259b0bad7671b6a7f9c50e7b47e
spam4kev <spam4kev@gmail.com>
2016-01-05 11:22:54 -0500

    wip

--- pxe-entrypoint.sh
+++ pxe-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+wget http://boot.ipxe.org/undionly.kpxe
+wget http://10.11.11.59:8150/api/microkernel/bootstrap?nic_max=1 -O bootstrap.ipxe
     dnsmasq  \
 		--dhcp-match=IPXEBOOT,175 \
 		--dhcp-boot=net:IPXEBOOT,bootstrap.ipxe \
