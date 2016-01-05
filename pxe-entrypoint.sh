5bd2f6c6cde80faea08862b2ba1409cd0a6d2733
spam4kev <spam4kev@gmail.com>
2016-01-05 14:45:46 -0500

    wip

--- pxe-entrypoint.sh
+++ pxe-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget --tries=0 http://boot.ipxe.org/undionly.kpxe
-wget --tries=0 http://10.11.11.59:8150/api/microkernel/bootstrap?nic_max=1 -O bootstrap.ipxe
+myIP=$(ip addr show dev eth0 | awk -F '[ /]+' '/global/ {print $3}')
+mySUBNET=$(echo $myIP | cut -d '.' -f 1,2,3)
 dnsmasq  \
 	--dhcp-match=IPXEBOOT,175 \
 	--dhcp-boot=net:IPXEBOOT,bootstrap.ipxe \
@@ -10,5 +11,5 @@ dnsmasq  \
 	--port=0 \
 	--log-dhcp \
 	--bind-dynamic \
-	--dhcp-range=10.11.11.201,10.11.11.202 \
+        --dhcp-range=$mySUBNET.201,$mySUBNET.202,255.255.255.0,1h \
 	--no-daemon
